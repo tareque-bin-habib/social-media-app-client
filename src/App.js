fd98e3c38b808dc84dfc46932a99f5d50efd8d29
tareque-bin-habib <shad.tbh@gmail.com>
2023-01-28 19:08:49 +0600

    background ready

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      Subscribe Zainkeeps code
-    </div>
+      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
+      <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
+    </div >
   );
 }
 
