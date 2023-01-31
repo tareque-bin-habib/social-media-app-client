336ea49bc33677eaabfa2cfc76dd86acf7e951f8
tareque-bin-habib <shad.tbh@gmail.com>
2023-01-31 19:58:17 +0600

    add functionality

--- src/components/PostShare/PostShare.js
+++ src/components/PostShare/PostShare.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import profile from '../../img/profileImg.jpg'
 import './PostShare.css'
 import { HiOutlinePhotograph } from 'react-icons/hi';
 import { AiOutlinePlayCircle } from 'react-icons/ai';
 import { HiOutlineLocationMarker } from 'react-icons/hi';
 import { CiViewTimeline } from 'react-icons/ci';
+import { ImCross } from 'react-icons/im';
 
 
 const PostShare = () => {
+    const [image, setImage] = useState(null)
+    const imageRef = useRef()
+
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0]
+            setImage({
+                image: URL.createObjectURL(img)
+            })
+        }
+    }
+
     return (
         <div className='postShare'>
             <img src={profile} alt="" />
@@ -16,6 +29,7 @@ const PostShare = () => {
                 <div className='postOptions'>
                     <div className='option'
                         style={{ color: 'var(--photo)' }}
+                        onClick={() => imageRef.current.click()}
                     >
                         <HiOutlinePhotograph className='schedule' />
                         Photo
@@ -38,10 +52,24 @@ const PostShare = () => {
                         <CiViewTimeline className='schedule' />
                         Schedule
                     </div>
+                    <button className='button ps-button'>
+                        Share
+                    </button>
+                    <div style={{ display: 'none' }}>
+                        <input type="file" name='myImage' ref={imageRef} onChange={onImageChange} />
+                    </div>
                 </div>
+
+                {
+                    image && (
+                        <div className='previewImage'>
+                            <ImCross />
+                        </div>
+                    )
+                }
             </div>
 
-        </div>
+        </div >
     );
 };
 
