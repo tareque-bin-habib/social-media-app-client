abfb2ba4677e1263af0b482eac3407e323d4fce3
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-04 11:32:06 +0600

    profile started working

---
+++ src/components/ProfileLeft/ProfileLeft.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProfileLeft.css'
+
+const ProfileLeft = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProfileLeft;
---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfileLeft from '../../components/ProfileLeft/ProfileLeft';
+import './Profile.css'
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <ProfileLeft></ProfileLeft>
+        </div>
+    );
+};
+
+export default Profile;
